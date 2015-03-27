218808fa6f5fc87a95af723a3a92dabe33c612cf
joakimeliasson <jeliasson@hotmail.com>
2015-03-27 13:02:24 +0100

    added MVC structure

--- core/src/BallBuster/View/BallBusterView.java
+++ core/src/BallBuster/View/BallBusterView.java
@@ -1,4 +1,4 @@
-package BallBuster;
+package BallBuster.View;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -13,7 +13,7 @@ public class BallBusterView extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,7 +21,7 @@ public class BallBusterView extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+//		batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
--- desktop/src/BallBuster/desktop/DesktopLauncher.java
+++ desktop/src/BallBuster/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package BallBuster.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import BallBuster.BallBusterView;
+import BallBuster.View.BallBusterView;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
