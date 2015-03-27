8004f34d04fb59a379d7b92692c0ab3e83f45f6e
joakimeliasson <jeliasson@hotmail.com>
2015-03-27 12:32:33 +0100

    gradle project

---
+++ android/src/BallBuster/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package BallBuster.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import BallBuster.BallBusterView;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BallBusterView(), config);
+	}
+}
---
+++ desktop/src/BallBuster/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package BallBuster.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import BallBuster.BallBusterView;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BallBusterView(), config);
+	}
+}
