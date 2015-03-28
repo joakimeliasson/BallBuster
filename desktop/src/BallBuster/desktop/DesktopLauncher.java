67ed716a650aa0225bc4bb0056c93dddccf5b1b5
Jacob Lundberg <jacob.thureson@gmail.com>
2015-03-28 15:08:07 +0100

    Adjusting screen size

--- desktop/src/BallBuster/desktop/DesktopLauncher.java
+++ desktop/src/BallBuster/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package BallBuster.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import BallBuster.View.BallBusterView;
@@ -8,5 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new BallBusterView(), config);
-	}
+
+        config.height = Gdx.graphics.getHeight();
+        config.width = Gdx.graphics.getWidth();
+    }
 }
