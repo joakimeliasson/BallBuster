b4a745ee49df6488af83d89e1b6e191135ef14a8
Jacob Lundberg <jacob.thureson@gmail.com>
2015-03-30 20:11:46 +0200

    Added move directions to Ball

--- core/src/BallBuster/Model/Ball.java
+++ core/src/BallBuster/Model/Ball.java
@@ -1,5 +1,7 @@
 package BallBuster.Model;
 
+import com.badlogic.gdx.Gdx;
+
 import java.awt.*;
 
 /**
@@ -8,11 +10,13 @@ import java.awt.*;
 public class Ball {
 
     private int shield;
+    private float ballSpeed;
 
     private Player player;
     private Aura aura;
     private Map map;
     private Point position;
+
 
     public Ball(Player player, Aura aura, Map map) {
         this.player = player;
@@ -21,6 +25,19 @@ public class Ball {
 
         shield = 100;
         position = new Point(0,0);
+    }
+
+    public void moveRight() {
+        position.x += Gdx.graphics.getDeltaTime() * ballSpeed;
+    }
+    public void moveLeft() {
+        position.x -= Gdx.graphics.getDeltaTime() * ballSpeed;
+    }
+    public void moveUp() {
+        position.y += Gdx.graphics.getDeltaTime() * ballSpeed;
+    }
+    public void moveDown() {
+        position.y -= Gdx.graphics.getDeltaTime() * ballSpeed;
     }
 
     public void accelerateBall() {
@@ -41,6 +58,7 @@ public class Ball {
     }
 
     public int shieldDamage() {
+
         return 0;
     }
 
