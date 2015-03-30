4235cc12ee9655fa1fe8fd04e7c725a2f34eca3b
joakimeliasson <jeliasson@hotmail.com>
2015-03-30 15:52:00 +0200

    Model and Controller folders

---
+++ core/src/BallBuster/Controller/Controller.java
@@ -0,0 +1,7 @@
+package BallBuster.Controller;
+
+/**
+ * Created by Joakim on 2015-03-30.
+ */
+public class Controller {
+}
---
+++ core/src/BallBuster/Model/Ball.java
@@ -0,0 +1,7 @@
+package BallBuster.Model;
+
+/**
+ * Created by Joakim on 2015-03-30.
+ */
+public class Ball {
+}
