069e97ad88f9c432c1494de7bfbbd9c661a184f1
Jacob Lundberg <jacob.thureson@gmail.com>
2015-03-30 20:25:03 +0200

    Create method for ball

--- core/src/BallBuster/Model/BallFactory.java
+++ core/src/BallBuster/Model/BallFactory.java
@@ -1,8 +1,22 @@
 package BallBuster.Model;
 
+import java.awt.*;
+
 /**
  * Created by jacobth on 2015-03-30.
  */
 public class BallFactory {
-    
+    private Player player;
+    private Aura aura;
+    private Map map;
+
+    public BallFactory(Player player, Aura aura, Map map) {
+        this.player = player;
+        this.aura = aura;
+        this.map = map;
+    }
+
+    public Ball createBall() {
+        return new Ball(player, aura, map);
+    }
 }
