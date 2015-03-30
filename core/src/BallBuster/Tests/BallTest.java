0427275d815cf3b73f79cb92a0463368df1b7d6a
Jacob Lundberg <jacob.thureson@gmail.com>
2015-03-30 23:25:36 +0200

    Test ball move

---
+++ core/src/BallBuster/Tests/BallTest.java
@@ -0,0 +1,21 @@
+package BallBuster.Tests;
+
+/**
+ * Created by jacobth on 2015-03-30.
+ */
+import BallBuster.Model.Ball;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class BallTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+    @Test
+    public void testMoveRight() {
+        Ball  ball = new Ball(null,null,null);
+        //assertNotNull();
+    }
+}
