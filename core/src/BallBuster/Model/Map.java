272e3b64f985f0d36905e850cbac6bc2c1f51090
Johan Segerlund <iamtwiz@gmail.com>
2015-03-31 19:44:51 +0200

    Created map class

--- core/src/BallBuster/Model/Map.java
+++ core/src/BallBuster/Model/Map.java
@@ -1,7 +1,49 @@
 package BallBuster.Model;
 
+import java.util.ArrayList;
+
 /**
- * Created by jacobth on 2015-03-30.
+ * Created by Johan Segerlund on 2015-03-30.
  */
 public class Map {
+
+    private int HEIGHT;
+    private int WIDTH;
+    private ArrayList entities = new ArrayList<>();
+
+    public Map() {
+        new Map(100, 100, 2); //just for now
+    }
+
+    public Map(int height, int width, int amountOfPlayers) {
+        this.HEIGHT = height;
+        this.WIDTH = width;
+
+        //Put all entities that needs to be updated here, players, power up timer etc...
+        for(int i = 0; i < amountOfPlayers; i++) {
+            //entities.add(new Player(i, startlocatoin)); i = which player => player 0 uses wasd as keybinds?
+        }
+    }
+
+    /**
+     * Return the tile object at given position
+     * @param x coordinate (pixels?)
+     * @param y coordinate (pixels?)
+     * @return the tile
+     */
+    public Object getTile(int x, int y) {
+        return null;
+    }
+
+    /**
+     * Updates all content on the map.
+     */
+    public void update() {
+        for(int i = 0; i < entities.size();i++) {
+            //players.get(i).update();
+            //update power up timer?
+        }
+    }
+
+
 }
