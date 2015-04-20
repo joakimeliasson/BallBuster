2513ed2481f3d515e514e7ca7dd2659a4b2a7a6f
joakimeliasson <jeliasson@hotmail.com>
2015-04-20 09:18:55 +0200

    added aura sprite

--- core/src/BallBuster/Model/Aura.java
+++ core/src/BallBuster/Model/Aura.java
@@ -1,14 +1,25 @@
 package BallBuster.Model;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public class Aura {
     private boolean auraStatus;
+    Texture auraTexture;
+    Sprite auraSprite;
 
     public Aura(){
         this.auraStatus = false;
+
+        FileHandle shieldFileHandle = Gdx.files.internal("core/images/aura.png");
+        auraTexture = new Texture(shieldFileHandle);
+        auraSprite = new Sprite(auraTexture);
     }
 
     public void setAuraStatus(boolean b){
-        auraStatus = b;
+        this.auraStatus = b;
     }
 
     public boolean getAuraStatus(){
