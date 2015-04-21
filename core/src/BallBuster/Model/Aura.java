ada2529d840f7d3f9b48f0ca56871c2c61fd222b
Jacob Lundberg <jacob.thureson@gmail.com>
2015-04-21 16:55:47 +0200

    Added animation to Aura

--- core/src/BallBuster/Model/Aura.java
+++ core/src/BallBuster/Model/Aura.java
@@ -3,13 +3,28 @@ package BallBuster.Model;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Aura {
-    private boolean auraStatus;
+    private boolean auraStatus = true;
     Texture auraTexture;
     Sprite auraSprite;
     Ball b;
+
+    private Sprite sprite;
+
+    private static final int FRAME_COLS = 6;
+    private static final int FRAME_ROWS = 5;
+
+    private Animation walkAnimation;
+    private Texture walkSheet;
+    private TextureRegion[] walkFrames;
+    private TextureRegion currentFrame;
+
+    float stateTime;
 
     public Aura(){
 
@@ -40,4 +55,25 @@ public class Aura {
         return this.auraSprite;
     }
 
+    public void createAnimation() {
+        walkSheet = new Texture(Gdx.files.internal("core/images/animation.png"));
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/FRAME_COLS, walkSheet.getHeight()/FRAME_ROWS);
+        walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+        walkAnimation = new Animation(0.025f, walkFrames);
+        stateTime = 0f;
+    }
+    public void renderAnimation(SpriteBatch batch) {
+        stateTime += Gdx.graphics.getDeltaTime();
+        currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+        sprite = new Sprite(currentFrame);
+        sprite.setPosition((b.getBody().getPosition().x*100)-sprite.getWidth()/2, (b.getBody().getPosition().y*100)-sprite.getHeight()/2);
+        batch.draw(sprite, sprite.getX(), sprite.getY(), sprite.getOriginX(), sprite.getOriginY(),
+                sprite.getWidth(), sprite.getHeight(), sprite.getScaleX(), sprite.getScaleY(), sprite.getRotation());
+    }
 }
