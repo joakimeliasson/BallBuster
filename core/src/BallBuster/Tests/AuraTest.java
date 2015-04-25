0606dcbf783913ebe9e3178a6cf20370f8b11488
Jacob Lundberg <jacob.thureson@gmail.com>
2015-04-25 12:33:05 +0200

    Testing Aura

---
+++ core/src/BallBuster/Tests/AuraTest.java
@@ -0,0 +1,32 @@
+package BallBuster.Tests;
+
+import BallBuster.Model.Aura;
+import BallBuster.Model.Ball;
+import BallBuster.Model.Map;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by jacobth on 2015-04-25.
+ */
+public class AuraTest {
+
+    private Aura aura;
+
+    @Before
+    public void setUp() throws Exception {
+        aura = new Aura();
+    }
+    @Test
+    public void testSetAuraStatus() {
+        aura.setAuraStatus(true);
+        assertTrue(aura.getAuraStatus());
+    }
+    @Test
+    public void testSetPosition() {
+        aura.setPosition(5f, 4f);
+        assertTrue(aura.getX() == 5f && aura.getY() == 4f);
+    }
+}
