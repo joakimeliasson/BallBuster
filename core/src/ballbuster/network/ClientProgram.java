b47310286276d782269d5830f14cfbc236d061ee
joakimeliasson <jeliasson@hotmail.com>
2015-05-08 11:19:58 +0200

    added directory for kryonet

---
+++ core/src/ballbuster/network/ClientProgram.java
@@ -0,0 +1,7 @@
+package ballbuster.network;
+
+/**
+ * Created by Joakim on 2015-05-08.
+ */
+public class ClientProgram {
+}
---
+++ core/src/ballbuster/network/Network.java
@@ -0,0 +1,16 @@
+package ballbuster.network;
+
+import com.esotericsoftware.kryo.Kryo;
+
+
+
+/**
+ * Created by Joakim on 2015-05-08.
+ */
+public class Network {
+
+
+    public Network(){
+
+    }
+}
