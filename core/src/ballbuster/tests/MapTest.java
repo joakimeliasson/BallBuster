cc91791a655f107643e0c474ff269d3c9acbaa3f
Johan Segerlund <iamtwiz@gmail.com>
2015-05-28 10:32:41 +0200

    added mapTest

---
+++ core/src/ballbuster/tests/MapTest.java
@@ -0,0 +1,30 @@
+package ballbuster.tests;
+
+import ballbuster.model.Aura;
+import ballbuster.model.Map;
+import ballbuster.model.tile.Tile;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Johan Segerlund on 2015-05-28.
+ */
+public class MapTest {
+
+    private Map map;
+
+    @Before
+    public void setUp() throws Exception {
+        map = new Map();
+    }
+
+    @Test
+    public void testAddTile(){
+        Tile tile = new Tile(0f,1f);
+        map.addTile(tile);
+        assertNotNull(map.getTiles());
+    }
+}
